45dca2171e3050a9852d5cbc46cdc88d49343207
HammerPanzer92 <romdur59@gmail.com>
2023-06-18 11:42:57 +0200

    Router created with simple route -Middleware in place to treat request

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,11 @@ const port = 5000;
 
 const app = express();
 
+//Middleware for treating request
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/post", require('./routes/post.routes'));
+
+//Launch the server
 app.listen(port, () => console.log("Server listening at port " + port))
